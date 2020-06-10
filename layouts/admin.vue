2a1f210c91e0e98658a98accbc3e35c12e0442e4
artemij-petrovskij <petrovsky.artem.dev@gmail.com>
2020-06-10 19:09:08 +0300

    init project

---
+++ layouts/admin.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="admin">
+  <el-container :style="{height:'100%'}">
+    <el-aside width="200px">
+      Aside
+    </el-aside>
+    <el-main>
+      <nuxt />
+    </el-main>
+  </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .admin{
+    width:100%;
+    height:100vh;
+  }
+
+</style>
