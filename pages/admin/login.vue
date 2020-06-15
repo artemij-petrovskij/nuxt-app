75f18e3ea0fa615a8effc35bb46f5c07a13aa750
artemij-petrovskij <petrovsky.artem.dev@gmail.com>
2020-06-15 19:23:12 +0300

    com

--- pages/admin/login.vue
+++ pages/admin/login.vue
@@ -1,9 +1,34 @@
 <template>
-  <el-card :style="{width:'400px'}">Login</el-card>
+  <el-card shadow="always" :style="{width:'500px'}">
+    <el-form :model="controls" :rules="rules" ref="form" @submit.native.prevent="onSubmit">
+
+      <el-form-item label="Имя" prop="name">
+        <el-input v-model="name"></el-input>
+      </el-form-item>
+
+
+
+
+
+    </el-form>
+  </el-card>
 </template>
 
 <script>
 export default {
-  layout: 'empty'
-}
+  layout: "empty",
+  data() {
+    return {
+      loading: false,
+      controls: {
+        name: "",
+        text: ""
+      },
+      rules: {
+        name: [],
+        text: []
+      }
+    };
+  }
+};
 </script>
