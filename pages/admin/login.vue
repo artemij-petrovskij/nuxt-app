2620c9ca04704c8d5c4e35ac148d7e3f8175d28f
artemij-petrovskij <petrovsky.artem.dev@gmail.com>
2020-06-16 19:25:39 +0300

    prepare login page

--- pages/admin/login.vue
+++ pages/admin/login.vue
@@ -1,15 +1,19 @@
 <template>
   <el-card shadow="always" :style="{width:'500px'}">
     <el-form :model="controls" :rules="rules" ref="form" @submit.native.prevent="onSubmit">
+      <h1>Вход</h1>
 
-      <el-form-item label="Имя" prop="name">
-        <el-input v-model="name"></el-input>
+      <el-form-item label="Логин" prop="login">
+        <el-input v-model.trim="controls.login"></el-input>
       </el-form-item>
 
+      <el-form-item label="Пароль" prop="password">
+        <el-input v-model.trim="controls.password" type="password"></el-input>
+      </el-form-item>
 
-
-
-
+      <el-form-item>
+        <el-button type="primary" native-type="submit" round :loading="loading">Войти</el-button>
+      </el-form-item>
     </el-form>
   </el-card>
 </template>
@@ -21,14 +25,52 @@ export default {
     return {
       loading: false,
       controls: {
-        name: "",
-        text: ""
+        login: "",
+        password: ""
       },
       rules: {
-        name: [],
-        text: []
+        login: [
+          {
+            required: true,
+            message: "Введите логин",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: "Введите пароль",
+            trigger: "blur"
+          },
+          {
+            min: 2, message:'Пароль должен быть не менее 2-х символов', trigger: 'blur'
+          }
+        ]
       }
     };
+  },
+  methods: {
+    onSubmit() {
+      this.$refs.form.validate(valid =>{
+        if(valid){
+          this.loading = true
+            try{
+              const formData = {
+                login: this.controls.login,
+                password: this.controls.password
+              }
+
+            }catch (e){
+
+            }
+
+
+ console.log("Submit");
+
+        }
+      })
+
+    }
   }
 };
 </script>
